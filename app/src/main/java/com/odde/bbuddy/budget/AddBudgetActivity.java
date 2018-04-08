0e0c89ec17d35b3bb0fed98ab086f277cc1b456f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-04-08 16:37:52 +0800

    Pass test case of add budget page

---
+++ app/src/main/java/com/odde/bbuddy/budget/AddBudgetActivity.java
@@ -0,0 +1,20 @@
+package com.odde.bbuddy.budget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.odde.bbuddy.R;
+
+public class AddBudgetActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_add_budget);
+
+    }
+
+}
---
+++ app/src/main/java/com/odde/bbuddy/budget/BudgetsActivity.java
@@ -0,0 +1,25 @@
+package com.odde.bbuddy.budget;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.odde.bbuddy.R;
+
+
+public class BudgetsActivity extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_budgets,null);
+
+    }
+}
